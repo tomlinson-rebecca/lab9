dd0ad05940a9a7df31c52fa62f52759ce9d4e0cb
Beaky <retomlinson14@gmail.com>
2019-03-07 10:44:58 -0800

    Finished lab9

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -12,14 +12,16 @@ function initializePage() {
 
 // Function that is called when the server responds to the AJAX call
 function onServerResponse(data){
+
     var labElementId = getLabElementId(data.id);
     var labElement = $(labElementId);
+    console.log(labElement)
     labElement.css({'background': 'yellow'});
 }
 
 
 // Function to format the CSS selector
 function getLabElementId(id){
-    var labElementId = '#lab_' + id;
+    var labElementId = '#lab-' + id;
     return labElementId;
 }
